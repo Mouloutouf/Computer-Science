580b599584d7e6559b336f55fea3ad3753044c01
Mouloutouf <maxdurlon@gmail.com>
2022-07-31 01:50:21 +0200

    Linked List Reverse Exercice

---
+++ LtcdLinkedListReverse.cpp
@@ -0,0 +1,36 @@
+// Given A Linked List, Reverse it then return it.
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *current = head, *newHead = head;
+        while (current != nullptr && current->next != nullptr){
+            ListNode *first = current->next;
+            ListNode *second = first->next;
+            first->next = newHead;
+            current->next = second;
+            newHead = first;
+        }
+        return newHead;
+    }
+};
+
+class OtherSolution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *cur = head, *nxt = nullptr, *prev = nullptr;
+        while (cur != nullptr){
+            nxt = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = nxt;
+        }
+        return prev;
+    }
+};
