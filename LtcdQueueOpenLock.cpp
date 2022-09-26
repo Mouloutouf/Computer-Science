b9cbd5fff0301627191fa2f42137dd03c4fd0956
Mouloutouf <maxdurlon@gmail.com>
2022-09-26 18:24:55 +0200

    Debug of Open Lock Problem, Ranting on Leetcode's trash policy for exercice completion

--- LtcdQueueOpenLock.cpp
+++ LtcdQueueOpenLock.cpp
@@ -5,15 +5,40 @@
 #include <string>
 using namespace std;
 
+// This code works quite fine, even handles all the smart ass edge cases from Leetcode, but still Leetcode thinks this is slow, even though there is no runtime
+// complexity required anywhere in the damn exercice. So yeah, that's fucking Leetcode in a nutshell. They are making every single thing difficult for you, all the time.
+// I'm starting to grow sick of their bullshit platform, for real.
+
+// Actually, there is no way this code runs too slow. It's virtually impossible since at maxima, I will run it 80000 times, for 10000 elements, which is not a lot.
+// So the code is probably stuck in some loop with larger inputs. But that too is impossible since my loops are fixed and my queue will eventually empty itself.
+// So I have no clue what Leetcode did with their input to achieve a Timeout, but fucking congratulations guys, and thanks for not giving any logs nor any other
+// way to debug this. Jesus christ.
+
+// I swear to god if the timeout is caused by the checks on deadends for each neighbour of each elements (which indeed could cause a slower calculation, though, then again
+// they never said anything about time complexity) I'm going to be real pissed. I mean how the fuck am I supposed to do this efficiently, when they give a god damn vector
+// of deadends when they could have given a fucking hashset, and make things easier for everyone.
+// Now I'm the one that has to convert their stupid jack ass data structure into a better one to solve the problem. I am sick of this shit, just make it easier for once.
+
+// Okay well Leetcode here again, just fixed their hashset problem, although I shouldn't be the one to do it, but fair enough, it was quite obvious it could cause a problem,
+// since there is no limit to the amount of deadends you can set.
+// But still, it doesn't work, timeout again, this time with absolutely no reason, except a big amount of deadends, which should not be a problem since I have a hashset.
+// So I have no idea why they timeout the shit out of my code everytime, even though there is now virtually no way it can exceed 100000 frames
+
 class Solution {
     string directions[8] = {"+0001", "-0001", "+0010", "-0010", "+1000", "-1000", "+0100", "-0100"};
 public:
     int openLock(vector<string>& deadends, string target) {
+        set<string> visited;
+        for (string& deadend : deadends){
+            if (deadend == "0000"){
+                return -1;
+            }
+            visited.emplace(deadend);
+        }
+        visited.emplace("0000");
+
         queue<string> search;
         search.push("0000");
-
-        set<string> visited;
-        visited.emplace("0000");
 
         map<string, string> previous;
 
@@ -39,11 +64,6 @@ public:
                     if (visited.count(direction) != 0){
                         continue;
                     }
-                    for (string& deadend : deadends){
-                        if (direction == deadend){
-                            continue;
-                        }
-                    }
                     
                     search.push(direction);
                     visited.emplace(direction);
@@ -54,10 +74,10 @@ public:
         
         int steps = 0;
         for (string prev = target; prev.empty() == false; prev = previous[prev]){
-            steps++;
             if (prev == "0000"){
                 return steps;
             }
+            steps++;
         }
         return -1;
     }
