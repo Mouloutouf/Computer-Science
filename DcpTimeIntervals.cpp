161be71b6dad19a54d79e0231884329b19e7ba05
Maxence Droz <maxence.drozb@gmail.com>
2023-03-21 18:14:08 +0100

    AI Courses Images, Coding Exercise Time Intervals

---
+++ DcpTimeIntervals.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int RequiredRooms(std::vector<std::pair<int, int>> intervals)
+{
+    if (intervals.size() == 0)
+        return 0;
+
+    std::sort(intervals.begin(), intervals.end());
+
+    std::vector<int> thresholds;
+    int rooms = 1;
+    for (int i = 0; i < intervals.size(); ++i)
+    {
+        for (int j = 0; j < thresholds.size(); ++j)
+        {
+            if (intervals[i].first >= thresholds[j])
+                break;
+
+            rooms++;
+        }
+        
+        thresholds.push_back(intervals[i].second);
+    }
+}
