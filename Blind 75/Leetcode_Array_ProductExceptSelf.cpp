aafd9d7369f39537db5624d5b8652353bd5d1df1
Maxence Droz <maxence.drozb@gmail.com>
2024-01-31 12:35:59 +0100

    Solutions and Explanations for 2 Leetcode Array Problems

---
+++ Blind 75/Leetcode_Array_ProductExceptSelf.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+
+using namespace std;
+
+// Given an array nums, return an array such that each element at index i is equal to the product of every number except the number at i.
+// Example : [1,2,3,4], should return [24,12,8,6] which are the multiplications of every number of the array except the number at the same index.
+// This solution should run in O(n) time and without the use of the division operator.
+
+// This is complicated since the two obvious solutions are either to do a O(n2) double for loop in order to multiply everything except self, which is bad.
+// Or, to multiply every numbers once, then divide them each by the number at the index, in order to get the result, which is O(n).
+// But in cases where there are zeros in the array, the division starts to get very problematic, hence this solution.
+
+// The idea here is to use a logic of prefix and postfix multiplication, meaning that each product really is the product of what is before the number and what is after the number.
+// Knowing that, we can go through the array twice, in normal then in reverse order, and calculate the prefix and postfix as we go, and registering them directly in the result array,
+// to avoid using more memory than is necessary.
+// What we do is we first add the prefix inside the result array, on the first pass, then we multiply with the postfix inside the result array, on the second pass.
+// The way we avoid the actual number is quite easy, we initialize the fixes at 1, then we first register that fix in the result array as the fix for the current number,
+// and only afterwards do we multiply it with the current number, which will then serve as the fix for the next number. Therefore the end (or start) numbers of the array will just be ignored.
+
+// This is the correct solution, where we traverse from one side then from the other, calculating the prefix and postfix as we go for each number, and multiplying everything in the result array.
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums)
+    {
+        vector<int> result = vector<int>(nums.size());
+
+        int fix = 1;
+        for (int i =  0; i < nums.size(); ++i)
+        {
+            result[i] = fix;
+            fix *= nums[i];
+        }
+        fix = 1;
+        for (int j = nums.size() - 1; j >= 0; --j)
+        {
+            result[j] *= fix;
+            fix *= nums[j];
+        }
+
+        return result;
+    }
+};
--- Blind 75/Leetcode_Array_TopFrequentElements.cpp
+++ Blind 75/Leetcode_Array_TopFrequentElements.cpp
@@ -1,7 +1,19 @@
 #include <vector>
+#include <unordered_map>
 #include <map>
 #include <set>
 using namespace std;
+
+// Given an array nums, return the k most frequent elements, meaning the elements that appear the most in that array
+// Example : [1,1,1,2,2,3], k = 2, should return [1,2] because 1 and 2 are the elements that appear the most in that array
+
+// The idea to solve this problem, is to go through the array and count how many times we encounter each number, for that we can use a hashmap, which takes the num as key and count as value.
+// And then we need to register these numbers in an array where each index represents the count of a number, then store the numbers according to their count.
+// This will actually be an array of arrays, since several numbers may have the same count and will be registered at the same index.
+// To finish the solution, we go through that array in reverse order, starting with the biggest count, and then add to the result array the numbers we find, until we hit k elements, then return.
+
+// This first solution was quite inefficient since I was counting and registering the numbers at the same time, meaning I had to erase and re-insert numbers constantly inside of a set.
+// This is therefore quite slow.
 
 class Solution
 {
@@ -12,13 +24,13 @@ public:
             return {};
 
         map<int, int> numberCounts;
-        vector<set<int>> frequentElements = vector<set<int>>();
+        vector<set<int>> frequentElements = vector<set<int>>(nums.size() + 1, set<int>());
 
         for (int i = 0; i < nums.size(); ++i)
         {
             int num = nums[i];
-            int count = 0;
 
+            int count = 0;
             if (numberCounts.count(num))
                 count = numberCounts[num];
             
@@ -27,12 +39,56 @@ public:
             if (count > 0)
                 frequentElements[count].erase(num);
             
-            if (frequentElements.size() == count)
-                frequentElements.push_back(set<int>({num}));
-            else
-                frequentElements[count].insert(num);
+            frequentElements[count + 1].insert(num);
         }
 
+        vector<int> result = vector<int>();
+
+        for (int j = frequentElements.size() - 1; j >= 0; --j)
+        {
+            for (int element : frequentElements[j])
+            {
+                result.push_back(element);
+
+                if (result.size() == k)
+                    return result;
+            }
+        }
+        return result;
+    }
+};
+
+// This is the best solution of the bunch, where I first count all the numbers and put them in a map, then I go through them to register them in the array, this is way more efficient,
+// since I do not need to erase and register numbers again. I therefore don't need to use a set either and can use a standard array for the array of arrays.
+// Also, using an unordered map is more efficient since we don't need to have the numbers (the keys) in a certain order.
+// This is way faster.
+
+// However, IMPORTANT, this is a bit memory inefficient, since we initialize the array of arrays to have the size of our input array, since we know that at most, the input array will have
+// every single value be the same number, which means the count of that number will be at most the size of the array. Therefore our array of arrays will also have at most a max count of input array size.
+// This can be improved however by using a priority queue, which does not need to be initialized with a large size, and can store elements in order of priority.
+
+class GoodSolution
+{
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k)
+    {
+        if (nums.size() == 0)
+            return {};
+
+        unordered_map<int, int> counts;
+        vector<vector<int>> frequentElements = vector<vector<int>>(nums.size() + 1, vector<int>());
+
+        for (int i = 0; i < nums.size(); ++i)
+            counts[nums[i]]++;
+
+        for (auto& pair : counts)
+        {
+            int num = pair.first;
+            int count = pair.second;
+
+            frequentElements[count].push_back(num);
+        }
+        
         vector<int> result = vector<int>();
 
         for (int j = frequentElements.size() - 1; j >= 0; --j)
